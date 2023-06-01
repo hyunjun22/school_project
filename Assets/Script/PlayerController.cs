2853cbfa195c0efe71b253a5cef485a6cdf73814
hyunjun <donghj03@gmail.com>
2023-06-02 01:06:09 +0900

    Jump and Gravity setting

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,16 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private Vector2 velocity;
+
     private float inputAxis;
-    
-    public bool jumping { get; private set; }
     public float moveSpeed;     // 움직임 속도
-    public bool grounded;       // 땅에 붙었는지 안붙었는지
+    public bool grounded;       // 땅에 붙어있는지 안붙어있는지
+    public bool jumping;        // 점프했는지 안했는지
 
     //#.레이캐스트 사용 변수
     private LayerMask layerMask;
     private float radius = 0.25f;    // 반지름 길이(원)
-    private float distance = 0.85f; // 거리
+    private float distance = 0.85f;  // 거리
+
+    //#.점프&중력 사용 변수
+    private float maxJumpHeight = 5f; // 최대 점프 거리
+    private float maxJumpTime = 1f;   // 최대 점프 시간
+    private float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f); // 점프 파워
+    private float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2);
 
     void Awake()
     {
@@ -25,8 +31,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {   
-        PlayerMovement();
-        GroundedCheck();
+        PlayerMovement(); // 플레이어 움직임
+        GroundedCheck();  // 바닥에 붙었는 지 체크
+        Jump();           // 점프
+        ApplyGravity();   // 중력
     }
 
     //#.플레이어 움직임
@@ -59,14 +67,36 @@ public class PlayerController : MonoBehaviour
         // 바닥 충돌
         if(hit.collider){
             grounded = true;
-            Debug.Log("Hit");
         }            
         else
         {
             grounded = false; 
-            Debug.Log("Not Hit");
         }
     }
 
-    
+    //#.점프
+    void Jump(){
+
+        // 바닥에 붙어있지 않으면 실행X
+        if(!grounded)
+            return;
+
+        velocity.y = Mathf.Max(velocity.y, 0f); // 둘 중 큰 값을 반환한다.
+        jumping = velocity.y > 0f;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true; // 점프함
+        }
+    }
+
+    //#.중력 적용
+    void ApplyGravity(){
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
+    }
 }
