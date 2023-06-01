d9e0dc5665f534871b1573a9fca388fe6bac9b35
hyunjun <donghj03@gmail.com>
2023-06-02 00:36:48 +0900

    Grounded Check

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,11 +8,14 @@ public class PlayerController : MonoBehaviour
     private Vector2 velocity;
     private float inputAxis;
     
-    public CircleCollider2D ground_circle;
     public bool jumping { get; private set; }
-    public float moveSpeed;             // ??? ??
-    public bool grounded; // ??? ???? ? ????? ?
+    public float moveSpeed;     // 움직임 속도
+    public bool grounded;       // 땅에 붙었는지 안붙었는지
 
+    //#.레이캐스트 사용 변수
+    private LayerMask layerMask;
+    private float radius = 0.25f;    // 반지름 길이(원)
+    private float distance = 0.85f; // 거리
 
     void Awake()
     {
@@ -23,20 +26,47 @@ public class PlayerController : MonoBehaviour
     void Update()
     {   
         PlayerMovement();
+        GroundedCheck();
     }
 
+    //#.플레이어 움직임
     void PlayerMovement()
     {
-        rigid.velocity = velocity;
-
-
-        // ??? ???
+        // 좌우 움직임
         inputAxis = Input.GetAxisRaw("Horizontal");
         velocity.x = inputAxis * moveSpeed;
     
-        // ???? ??? (?? ???? ?)
-        if(inputAxis == 0){
+        rigid.velocity = velocity;
+
+        // 미끄러짐 방지 (땅에 붙어있을 때만)
+        if(inputAxis == 0 && grounded){
             velocity.x = 0;
         }
     }
+
+    //#.바닥에 붙어있는 지 체크
+    void GroundedCheck()
+    {
+        // 레이캐스트로 바닥과 충돌 감지
+        layerMask = LayerMask.GetMask("ground");
+        RaycastHit2D hit = Physics2D.CircleCast(rigid.position, radius, Vector2.down, distance, layerMask);
+
+        // 레이캐스트 범위 시각적으로 표시하기
+        Vector3 startLine = new Vector3(rigid.position.x, rigid.position.y - distance + radius, 0);
+        Vector3 endLine   = new Vector3(rigid.position.x, rigid.position.y - distance - radius, 0);
+        Debug.DrawLine(startLine, endLine, Color.red, 0.1f);
+
+        // 바닥 충돌
+        if(hit.collider){
+            grounded = true;
+            Debug.Log("Hit");
+        }            
+        else
+        {
+            grounded = false; 
+            Debug.Log("Not Hit");
+        }
+    }
+
+    
 }
