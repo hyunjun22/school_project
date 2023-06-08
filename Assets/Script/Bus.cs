f0a6607043f139bee0d306c2e8f19b66d702dc53
hyunjun <donghj03@gmail.com>
2023-06-09 05:03:08 +0900

    SavePoint and Stage2 Bus, secret block

--- Assets/Script/Bus.cs
+++ Assets/Script/Bus.cs
@@ -8,7 +8,10 @@ public class Bus : MonoBehaviour
     float startline, endline;
     public float walkSpeed; // 걷는 속도
     public float distance;  // 거리
+    public bool fake;       // 가짜인지
+
     float currentX;
+    Vector2 direction; // 방향
 
     void Awake()
     {
@@ -18,6 +21,9 @@ public class Bus : MonoBehaviour
     void Start(){
         rigid.velocity = Vector2.left * walkSpeed;
         currentX = transform.position.x;
+
+        if(fake)
+            this.gameObject.tag = "FakeBus";
     }
 
     void Update()
@@ -28,11 +34,13 @@ public class Bus : MonoBehaviour
         // 좌우로 움직이기
         if(startline > transform.position.x)
         {
-            rigid.velocity = Vector2.right * walkSpeed;
+            direction = Vector2.right;
+            rigid.velocity = direction * walkSpeed;
         }
         else if(endline < transform.position.x)
         {
-            rigid.velocity = Vector2.left * walkSpeed;
+            direction = Vector2.left;
+            rigid.velocity = direction * walkSpeed;
         }
 
         // 좌우 반전
@@ -43,5 +51,17 @@ public class Bus : MonoBehaviour
             transform.eulerAngles = new Vector3(0, 180f, 0); // 180도 회전
         }
                 
+    }
+
+    // 좌우 전환
+    void ChangeMovement(){
+        direction *= -1f;
+        rigid.velocity = direction * walkSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Bus" || other.gameObject.tag == "FakeBus"){
+            ChangeMovement();
+        }
     }
 }
