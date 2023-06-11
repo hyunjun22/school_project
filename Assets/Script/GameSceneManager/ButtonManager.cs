742d9f4660068b752d39137cf252d51b74127c2e
hyunjun <donghj03@gmail.com>
2023-06-12 03:00:49 +0900

    Leave button and Restart button and cloud setting

--- Assets/Script/GameSceneManager/ButtonManager.cs
+++ Assets/Script/GameSceneManager/ButtonManager.cs
@@ -4,16 +4,27 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
     public Button retryButton;
+    public Button RestartButton;
     public GameObject Board;
 
     void Start()
     {
         Connect();
+        Board.SetActive(false);
+    }
+
+    void Update()
+    {
+        //#.설정 버튼
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            SettingButton();
+        }
     }
 
     //#.버튼 연결
     void Connect(){
         retryButton.onClick.AddListener(() => GameManager.Instance.DeadSceneLoad());
+        RestartButton.onClick.AddListener(() => GameManager.Instance.GameSceneLoad());
     }
 
     //#.설정 버튼
@@ -23,5 +34,11 @@ public class ButtonManager : MonoBehaviour
         } else {
             Board.SetActive(true);
         }
+    }
+
+    public void leaveButton()
+    {
+        Debug.Log("leave");
+        Application.Quit();
     }
 }
